60f257b093cbe6f48d2d841403e829b8da345827
Whon1980-DL <supitpongw@hotmail.com>
2025-03-02 16:48:03 +0000

    Add Procfile for Heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9&1sz(7(u52#ix(+kmq_5vab@=#f_h(+tef5@*u4xt8b14h9+!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
