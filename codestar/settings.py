78b3e001bbb05c13ba06d65ab2d209d96c27c33f
Whon1980-DL <supitpongw@hotmail.com>
2025-03-13 05:16:04 +0000

    added authentication

--- codestar/settings.py
+++ codestar/settings.py
@@ -42,10 +42,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'blog',
     'about',
 ]
+
+# The redirection URLs are also added so that after we've logged in or logged out, 
+# the site will automatically redirect us to the home page.
+
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -56,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'codestar.urls'
@@ -115,6 +127,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# We are not using email verification in this project, so this line informs Django not to expect it. 
+# Without this line, you would get Internal Server errors (code 500) during login and registration.
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
